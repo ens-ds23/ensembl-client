3944e08fbdb751e3cf761fb5bd275b350667f2b4
Andrey Azov <andrey@ebi.ac.uk>
2019-04-17 10:21:53 +0100

    Add layout to the page

--- src/ensembl/src/root/Root.tsx
+++ src/ensembl/src/root/Root.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 import Main from 'src/demo-test/demo1/main/Main';
 
+import styles from './Root.scss';
+
 const Root = () => (
-  <div>
-    <Main />
+  <div className={styles.root}>
+    <div className={styles.example}>
+      <h1>Example</h1>
+      <Main />
+    </div>
+    <div className={styles.exercise}>
+      <h1>Exercise</h1>
+      Here is where our code will go
+    </div>
   </div>
 );
 
